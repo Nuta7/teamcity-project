b2529a73903c4e5a930194e6c7f841a665efaa39
ashmalko <annashmalko5@gmail.com>
2026-01-18 18:34:01 +0100

    Added api-tests

---
+++ src/test/java/com/example/teamcity/api/DummyTest.java
@@ -0,0 +1,21 @@
+package com.example.teamcity.api;
+
+import com.example.teamcity.api.models.User;
+import com.example.teamcity.api.spec.Specifications;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.testng.annotations.Test;
+
+public class DummyTest extends BaseApiTest {
+
+    @Test
+    public void userShouldBeAbleGetAllProjects() {
+        RestAssured
+                .given()
+                .spec(Specifications.getSpec()
+                        .authSpec(User.builder()
+                                .username("admin").password("admin")
+                                .build()))
+                .get("/app/rest/projects");
+    }
+}
