d659633b7def1ab3bae80acd63833d3ba1109c4a
ashmalko <annashmalko5@gmail.com>
2026-01-28 22:19:19 +0000

    Improved the BuildTypeTest.java

--- src/main/java/com/example/teamcity/api/models/Project.java
+++ src/main/java/com/example/teamcity/api/models/Project.java
@@ -1,11 +1,12 @@
 package com.example.teamcity.api.models;
 
+
 import com.example.teamcity.api.annotations.Random;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Builder
 @Data
@@ -13,9 +14,9 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Project extends BaseModel {
+    @Random
     private String id;
     @Random
     private String name;
-    @Builder.Default
-    private String locator = "_Root";
+    private String locator;
 }
