f17dafcea130544af98bcb2644bb61726ac76667
ashmalko <annashmalko5@gmail.com>
2026-02-03 14:57:39 +0200

    added WireMock

--- src/test/java/com/example/teamcity/api/DummyTest.java
+++ src/test/java/com/example/teamcity/api/DummyTest.java
@@ -1,9 +1,5 @@
 package com.example.teamcity.api;
 
-import com.example.teamcity.api.models.User;
-import com.example.teamcity.api.spec.Specifications;
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
 import org.testng.annotations.Test;
 
 public class DummyTest extends BaseApiTest {
