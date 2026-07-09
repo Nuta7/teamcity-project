212f4d56c4320f4ada478fbf6e45b914fa9d8405
ashmalko <annashmalko5@gmail.com>
2026-07-09 15:42:15 +0200

    added ci

--- src/test/java/com/example/teamcity/ui/BaseUiTest.java
+++ src/test/java/com/example/teamcity/ui/BaseUiTest.java
@@ -20,7 +20,7 @@ public class BaseUiTest extends BaseTest {
         Configuration.baseUrl = "http://" + Config.getProperty("host");
         // НЕ ПИШИТЕ UI ТЕСТЫ С ЛОКАЛЬНЫМ БРАУЗЕРОМ
         // А ПОТОМ ЗАПУСКАЕТЕ НА REMOTE BROWSER
-        //Configuration.remote = Config.getProperty("remote");
+        Configuration.remote = Config.getProperty("remote");
         Configuration.browserSize = Config.getProperty("browserSize");
         Configuration.pageLoadStrategy = "none";
 
