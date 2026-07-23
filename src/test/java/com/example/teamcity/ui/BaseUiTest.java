8c2b4e990ce13e9f2938ee67da98be69f8db9fba
ashmalko <annashmalko5@gmail.com>
2026-07-23 15:44:42 +0200

    added ci

--- src/test/java/com/example/teamcity/ui/BaseUiTest.java
+++ src/test/java/com/example/teamcity/ui/BaseUiTest.java
@@ -2,11 +2,13 @@ package com.example.teamcity.ui;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import com.example.teamcity.BaseTest;
 import com.example.teamcity.api.config.Config;
 import com.example.teamcity.api.enums.Endpoint;
 import com.example.teamcity.api.models.User;
 import com.example.teamcity.ui.pages.LoginPage;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeSuite;
@@ -25,6 +27,11 @@ public class BaseUiTest extends BaseTest {
         Configuration.pageLoadStrategy = "none";
 
         Configuration.browserCapabilities.setCapability("selenoid:options", Map.of("enableVNC", false, "enableLog", false));
+
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide()
+                .screenshots(true)
+                .savePageSource(true)
+                .includeSelenideSteps(true));
 
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--no-sandbox");
